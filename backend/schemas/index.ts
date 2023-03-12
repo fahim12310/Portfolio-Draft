221e81f0845a84a5f3568388db556f02302e26f5
Fahim123100 <u01fi21@abdn.ac.uk>
2023-03-12 22:27:01 +0000

    Backend sorted

---
+++ backend/schemas/index.ts
@@ -0,0 +1,18 @@
+import testimonial from './testimonials'
+import about from './abouts'
+import brands from './brands'
+import contact from './contact'
+import experience from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import work from './works'
+export const schemaTypes = [
+  work,
+  testimonial,
+  brands,
+  about,
+  skills,
+  workExperience,
+  experience,
+  contact,
+]
